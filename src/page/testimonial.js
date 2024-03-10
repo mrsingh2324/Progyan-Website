3fd14b762242f69d7b7c0053b76a224ec48e6602
aasn0119 <abhitrivedi119@gmail.com>
2024-03-10 12:04:15 +0530

    made changes in footer and testimonials section

--- src/page/testimonial.js
+++ src/page/testimonial.js
@@ -1,49 +1,47 @@
 import React from "react";
 import testimonialsData from "../data/info/testimonials.json";
 import "../styles/style.css";
-import Avatar from "../data/images/avatar3.jpg";
-import Avatar2 from "../data/images/avatar2.jpg";
-import Avatar3 from "../data/images/avatar1.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Testimonials = () => {
   return (
-    <section className="container testimonial_container swiper mySwiper p-0">
+    <section className="container testimonial_container p-0">
       <div className="heading h-[10%] flex items-center justify-center p-8">
         <span className="max-w-2xl mb-4 text-xl md:text-4xl font-extrabold">
           What our Student Says
         </span>
       </div>
-      <div className="swiper-wrapper flex gap-10 flex-col md:flex-row">
-        <article className="testimonial swiper-slide">
-          <div className="avatar">
-            <img src={Avatar} />
-          </div>
-          <div className="testimonial_info">
-            <h5>Rahul Sachdev</h5>
-            <small>Data Science <br/> (Lovely prfessional University)</small>
-          </div>
-          <div className="testimonial_body bg-black ml-4">
-            <p>
-              It is a unique and better way to learn my domain Data Science with ChatGPT 4.0, Hugging Face, Datacamp Workspacee AI all at once.
-            </p>
-          </div>
-        </article>
-        <article className="testimonial swiper-slide">
-          <div className="avatar">
-            <img src={Avatar2} />
-          </div>
-          <div className="testimonial_info">
-            <h5>Md. Danish Raza </h5>
-            <small>Data Science <br/> (Lovely prfessional University)</small>
-          </div>
-          <div className="testimonial_body bg-black">
-            <p>
-              I have learned a lot from this platform. It's a great platform to learn and enhance your skills.
-            </p>
-          </div>
-        </article>
-      </div>
-      <div className="swiper-pagination"></div>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        className="swiper-container flex gap-10 md:flex-row"
+      >
+        {testimonialsData.map((testimonial, index) => (
+          <SwiperSlide key={index} className="testimonial">
+            <div className="avatar">
+              <img
+                src={require(`../data/images/${testimonial.avatar}`)}
+                alt={`Avatar ${index + 1}`}
+              />
+            </div>
+            <div className="testimonial_info">
+              <h5>{testimonial.name}</h5>
+              <small>
+                {testimonial.role} <br /> ({testimonial.university})
+              </small>
+            </div>
+            <div className="testimonial_body bg-black ml-4">
+              <p>{testimonial.message}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };

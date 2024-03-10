ce07314e3ee5122cd029800a76302d5e49613455
aasn0119 <abhitrivedi119@gmail.com>
2024-03-10 12:33:57 +0530

    Changes in testimonials, style and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,20 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-700 p-2">
+    <nav className="p-2">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-        <Link to="/" className="flex items-center text-start gap-0 line-h-0 p-1 ">
-          <img src={logo} alt="Logo" className="w-[60px] flex text-white transition-all duration-300" />
-          <span className="text-lg font-bold">Progyan <br /> Educations</span>
+        <Link
+          to="/"
+          className="flex items-center text-start gap-0 line-h-0 p-1 "
+        >
+          <img
+            src={logo}
+            alt="Logo"
+            className="w-[60px] flex text-white transition-all duration-300"
+          />
+          <span className="text-lg font-bold">
+            Progyan <br /> Educations
+          </span>
         </Link>
         <button
           type="button"
@@ -43,28 +52,60 @@ const Navbar = () => {
             <FiMenu className="w-5 h-5 text-white " />
           )}
         </button>
-        <div className={`w-full md:block md:w-auto ${menuOpen ? '' : 'hidden'}`} id="navbar-default">
+        <div
+          className={`w-full md:block md:w-auto ${menuOpen ? "" : "hidden"}`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border md:p-2 md:px-4 border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700">
             <li>
-              <Link to="/" className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={handleMenuClick} >Home</Link>
-            </li >
+              <Link
+                to="/"
+                className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white"
+                onClick={handleMenuClick}
+              >
+                Home
+              </Link>
+            </li>
             <li>
-              <Link to="/about" className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={handleMenuClick} >About</Link>
-            </li >
+              <Link
+                to="/about"
+                className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white"
+                onClick={handleMenuClick}
+              >
+                About
+              </Link>
+            </li>
             <li>
-              <Link to="/courses" className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={handleMenuClick} >Courses</Link>
-            </li >
+              <Link
+                to="/courses"
+                className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white"
+                onClick={handleMenuClick}
+              >
+                Courses
+              </Link>
+            </li>
             <li>
-              <Link to="/contact" className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white" onClick={handleMenuClick}>Contact</Link>
-
-            </li >
+              <Link
+                to="/contact"
+                className="block py-2 px-3 md:border-0 md:hover:text-blue-700 md:p-0 text-white"
+                onClick={handleMenuClick}
+              >
+                Contact
+              </Link>
+            </li>
             <li>
-              <Link to="/register" className="block px-3 py-2 border border-transparent text-white transition-all duration-300 rounded font-bold hover:bg-gray-100 md:hover:text-black " onClick={handleMenuClick}>Join Now</Link>
-            </li >
-          </ul >
-        </div >
-      </div >
-    </nav >
+              <Link
+                to="/register"
+                className="block px-3 py-2 border border-transparent text-white transition-all duration-300 rounded font-bold hover:bg-gray-100 md:hover:text-black "
+                onClick={handleMenuClick}
+              >
+                Join Now
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
